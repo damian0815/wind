028abc9ce4a69e37e428599ef5a48ec282bf9cd4
damianmac <damianmac@7370d09c-0d9e-4c44-b511-94199c3f06bf>
2008-08-07 01:53:41 +0000

    gpl stuff
    git-svn-id: http://damian.dreamhosters.com/svn/projects/oespacograss@137 7370d09c-0d9e-4c44-b511-94199c3f06bf

--- src/testApp.h
+++ src/testApp.h
@@ -1,3 +1,26 @@
+/*
+
+ Copyright 2008 Damian Stewart
+
+ Distributed under the terms of the GNU General Public License v3
+
+ This file is part of Wind.
+ 
+ Wind is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Wind is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Wind.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef _TEST_APP
 #define _TEST_APP
 
